8311e3a6988d4fb97287dd37f4273cedd3867ef4
treesacherian <150167904+treesacherian@users.noreply.github.com>
2024-04-21 22:10:30 +0100

    Add files via upload

---
+++ PersonalProjectBE/src/test/java/com/lbg/PersonalProject/PersonalProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lbg.PersonalProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
