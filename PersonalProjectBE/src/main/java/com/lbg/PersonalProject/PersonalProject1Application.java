bbc581f0652210dbbbb48dcea612c71a887a9da2
treesacherian <150167904+treesacherian@users.noreply.github.com>
2024-04-21 22:07:29 +0100

    Add files via upload

---
+++ PersonalProjectBE/src/main/java/com/lbg/PersonalProject/PersonalProject1Application.java
@@ -0,0 +1,13 @@
+package com.lbg.PersonalProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalProject1Application.class, args);
+	}
+
+}
